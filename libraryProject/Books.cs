7812d6d1f1886eaae4041c6d9c476be3fc7f7b4d
Elijah Mivedor <elijahmivedor@gmail.com>
2018-11-13 10:45:23 +0000

    charlesmve update/commit

---
+++ libraryProject/Books.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryProject
+{
+    public class Books
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Year { get; set; }
+        public string Publisher { get; set; }
+        public Int16 Index { get; set; }
+        public Int16 Copies { get; set; }
+        public Int16 CheckedOut { get; set; }
+        public Int16 BookCount { get; set; }
+        public string Resource { get; set; }
+        public string Subtitle { get; set; }
+        public string Series { get; set; }
+        public Int16 TimesCheckedOut { get; set; }
+        public string Section { get; set; }
+        public string CallNumber { get; set; }
+
+
+    }
+}
--- libraryProject/Program.cs
+++ libraryProject/Program.cs
@@ -4,31 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using libraryProject;
 
 namespace LibraryProject
 {
-    public class Books
-    {
-        public string Isbn { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Year { get; set; }
-        public string Publisher { get; set; }
-        public Int16 Index { get; set; }
-        public Int16 Copies { get; set; }
-        public Int16 CheckedOut { get; set; }
-        public Int16 BookCount { get; set; }
 
-    }
     class Program
     {
-        static void Main()
+        public static void Main()
         {
             List<Books> Catalog = new List<Books>();
             string line = " ";
             using (StreamReader file = new StreamReader(@"D:\c#\libraryProject\booklist2.csv"))
             {
-               for(Int16 index =0; line !=null;index++)
+                for (Int16 index = 0; line != null; index++)
                 {
                     line = file.ReadLine();
                     if (line == null)
@@ -40,26 +29,92 @@ namespace LibraryProject
                     Books book = new Books();
                     book.Index = index;
                     Console.WriteLine(book.Index);
-                    book.Isbn = parts[0];
+                    book.Resource = parts[0];
                     book.Title = parts[1];
-                    book.Author = parts[2];
-                    book.Year = parts[3];
-                    book.Publisher = parts[4];
-                    book.Copies = Convert.ToInt16(parts[5]);
+                    book.Subtitle = parts[2];
+                    book.Series = parts[3];
+                    book.Section = parts[4];
+                    book.CallNumber = parts[5];
+                    book.Author = parts[6];
+                    book.Publisher = parts[7];
                     Catalog.Add(book);
                 }
+
             }
             Console.WriteLine(Catalog.Count);
-//           Catalog.ForEach(el => Console.WriteLine(Catalog[].Title));
-//           Console.WriteLine(Catalog.Sort);
+            //           Console.WriteLine(Catalog.Sort);
             Console.WriteLine(Catalog[1].Author);
             Console.WriteLine(Catalog[2].Copies);
             Console.WriteLine(Catalog[3].Publisher);
-            Catalog.ForEach(el => Catalog[el].Title);
+            AddBook();
             Console.ReadLine();
 
         }
+        public static void AddBook()
+        {
+            string line = "  ";
+            using (StreamWriter file = new StreamWriter(@"D:\c#\libraryProject\booklist2.csv"))
+            {
+                for (Int16 index = 0; line != null; index++)
+                {
+                    string[] parts = new string[] { "", "", "", "", "", "", "", "" };
+                    Console.WriteLine(line);
+                    Books book = new Books();
+                    Console.WriteLine();
+                    Console.Write("Resource:");
+                    book.Resource = Console.ReadLine();
+                    Console.Write("Title:");
+                    book.Title = Console.ReadLine();
+                    Console.Write("SubTitle:");
+                    book.Subtitle = Console.ReadLine();
+                    Console.Write("Series:");
+                    book.Series = Console.ReadLine();
+                    Console.Write("Section:");
+                    book.Section = Console.ReadLine();
+                    Console.Write("CallNumber:");
+                    book.CallNumber = Console.ReadLine();
+                    Console.Write("Author:");
+                    book.Author = Console.ReadLine();
+                    Console.Write("Publisher:");
+                    book.Publisher = Console.ReadLine();
+                    parts[0] = book.Resource;
+                    parts[1] = book.Title;
+                    parts[2] = book.Subtitle;
+                    parts[3] = book.Series;
+                    parts[4] = book.Section;
+                    parts[5] = book.CallNumber;
+                    parts[6] = book.Author;
+                    parts[7] = book.Publisher;
+                    file.WriteLine(parts[0] + "," + parts[1] + "," + parts[2] + "," + parts[3] + "," + parts[4] + "," + parts[5] + "," + parts[6] + "," + parts[7]);
+                }
+
+            }
+            /*
+            Books book = new Books();
+            Console.WriteLine();
+            Console.Write("Resource:");
+            book.Resource = Console.ReadLine();
+            Console.Write("Title:");
+            book.Title = Console.ReadLine();
+            Console.Write("SubTitle:");
+            book.Subtitle = Console.ReadLine();
+            Console.Write("Series:");
+            book.Series = Console.ReadLine();
+            Console.Write("Section:");
+            book.Section = Console.ReadLine();
+            Console.Write("CallNumber:");
+            book.CallNumber = Console.ReadLine();
+            Console.Write("Author:");
+            book.Author = Console.ReadLine();
+            Console.Write("Publisher:");
+            book.Publisher = Console.ReadLine();*/
+            Console.ReadLine();
+        }
+        public static void DeleteBook()
+        {
+            Books catalog = new Books();
+            catalog = null;
+        }
     }
 }
-
 
